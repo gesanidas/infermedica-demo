dff6b964df599862324b47d294198db1b6c4b73a
gesanidas <gesanidas@gmail.com>
2017-07-23 19:20:16 +0300

    worked on diagnosis activity

--- app/src/main/java/com/gesanidas/housemd/MainActivity.java
+++ app/src/main/java/com/gesanidas/housemd/MainActivity.java
@@ -254,17 +254,17 @@ public class MainActivity extends AppCompatActivity implements SymptomAdapter.Li
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_manage)
+        {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
 
-        } else if (id == R.id.nav_slideshow) {
+        }
+        else if (id == R.id.nav_share)
+        {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_send)
+        {
 
         }
 
